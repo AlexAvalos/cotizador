7bedc1eb32c340308346013b56c3f26472cb00c2
AlexAvalos <32860764+AlexAvalos@users.noreply.github.com>
2017-10-18 00:14:26 -0500

    Otra impresión de pantalla

--- Cotizador.java
+++ Cotizador.java
@@ -1,6 +1,7 @@
 public class Cotizador{
     public static void main(String[] args) {
       System.out.println("Que pedo Git");
-      System.out.println("Esto es para otro 'commit'");
+      System.out.println("Esto es para otro 'commit' 2");
+      System.out.println("");
     }
 }
